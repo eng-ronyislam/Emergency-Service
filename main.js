f0693d256dc2d87edaab1d407e5a8a569afa4604
eng-ronyislam <eng.ronyislam@gmail.com>
2025-08-27 23:21:05 +0600

    Added Call Buttons Functionalities

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+let hertCount = 0;
+const hartCounter = getElement("hart-counter");
+const coinCounter = getElement("coin-counter");
+let coinCount = Number(coinCounter.innerText);
+
+getElement("cards-box").addEventListener("click", function (e) {
+  if (e.target.className.includes("hert-btn")) {
+    hertCount++;
+    hartCounter.innerText = hertCount;
+  } else if (e.target.className.includes("call-btn")) {
+    const cartBtn = e.target;
+    const title =
+      cartBtn.parentNode.parentNode.children[1].children[0].innerText;
+
+    const number =
+      cartBtn.parentNode.parentNode.children[2].children[0].innerText;
+
+    coinCount = coinCount - 20;
+    if (coinCount >= 0) {
+      coinCounter.innerText = coinCount;
+      alert(
+        "আপনি {number} নম্বরে কল করতে যাচ্ছেন।".replace("{number}", number)
+      );
+      const cartContainer = getElement("cart-container");
+      const newCart = document.createElement("div");
+      newCart.innerHTML = `
+      <div class="bg-blue-50 p-4 rounded-lg mb-4">
+            <div class="flex justify-between items-center mb-2">
+              <h3 class="text-lg">${title}</h3>
+              <span class="text-gray-500">10:30 AM</span>
+            </div>
+            <p class="text-2xl font-bold">${number}</p>
+            
+          </div>
+    `;
+      cartContainer.appendChild(newCart);
+    } else {
+      alert("আপনার কয়েন শেষ।");
+      return;
+    }
+  }
+});
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("cart-container").innerHTML = "";
+});
